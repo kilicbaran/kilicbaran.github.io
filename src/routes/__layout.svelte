7a79d46b8a1cf5a37ff7030346fadaf8475f85de
Baran Kılıç <baran2kilic@gmail.com>
2021-08-19 17:10:25 +0300

    Add manifest-src to Content-Security-Policy meta tag

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,7 +4,7 @@
 </script>
 <svelte:head>
     {#if !dev}
-        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; connect-src 'self'; frame-src https://render.githubusercontent.com; img-src https://* 'self'; media-src 'self'; style-src https://rsms.me/ https://fonts.googleapis.com https://github.githubassets.com/ 'unsafe-inline' 'self'; font-src https://rsms.me/ https://fonts.gstatic.com;  script-src 'self' 'unsafe-inline'; worker-src 'self';">
+        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; connect-src 'self'; frame-src https://render.githubusercontent.com; img-src https://* 'self'; media-src 'self'; style-src https://rsms.me/ https://fonts.googleapis.com https://github.githubassets.com/ 'unsafe-inline' 'self'; font-src https://rsms.me/ https://fonts.gstatic.com; manifest-src 'self';  script-src 'self' 'unsafe-inline'; worker-src 'self';">
     {/if}
 </svelte:head>
 <slot></slot>
