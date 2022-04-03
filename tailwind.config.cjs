399f09b6238af136c1d99fc837177aee3f2a30ae
Baran Kılıç <baran2kilic@gmail.com>
2022-04-04 00:03:57 +0300

    Remove unnecessary code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const iOSHeight = require("@rvxlab/tailwind-plugin-ios-full-height");
-const colors = require('tailwindcss/colors.js');
 
 module.exports = {
     content: [
@@ -16,36 +15,6 @@ module.exports = {
                 ],
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
-            // colors: {
-            //     blueGray: colors.blueGray,
-            //     amber: colors.amber,
-            //     lime: colors.lime,
-            //     cyan: colors.cyan,
-            //     rose: colors.rose,
-            //     sky: colors.sky,
-            //     orange: colors.orange,
-            /*blueGray: colors.blueGray,
-            coolGray: colors.coolGray,
-            gray: colors.gray,
-            trueGray: colors.trueGray,
-            warmGray: colors.warmGray,
-            red: colors.red,
-            orange: colors.orange,
-            amber: colors.amber,
-            lime: colors.lime,
-            green: colors.green,
-            emerald: colors.emerald,
-            teal: colors.teal,
-            cyan: colors.cyan,
-            sky: colors.sky,
-            blue: colors.blue,
-            indigo: colors.indigo,
-            violet: colors.violet,
-            purple: colors.purple,
-            fuchsia: colors.fuchsia,
-            pink: colors.pink,
-            rose: colors.rose,*/
-            // },
             height: {
                 18: "4.5rem"
             },
