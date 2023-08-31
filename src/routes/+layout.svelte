035ee82b14404d50d59237da5036b305fd2fdcd3
Baran Kılıç <baran2kilic@gmail.com>
2023-08-31 11:39:14 +0300

    Add Google Tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,22 @@
 <script>
 	import '../app.css';
+	import { dev } from '$app/environment';
 </script>
+
+<svelte:head>
+	<!-- Google tag (gtag.js) -->
+	{#if !dev}
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-90M94T4S8S"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag() {
+				dataLayer.push(arguments);
+			}
+			gtag('js', new Date());
+
+			gtag('config', 'G-90M94T4S8S');
+		</script>
+	{/if}
+</svelte:head>
 
 <slot />
